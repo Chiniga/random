45bbc5f4d8db6f96c2d36b02a1f4837b779d4c5a
David <david.graves.2011@gmail.com>
2016-10-22 11:32:50 +0800

    Initial Commit

---
+++ app/src/main/java/com/graves/random/FullscreenActivity.java
@@ -0,0 +1,22 @@
+package com.graves.random;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class FullscreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_fullscreen);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+    }
+}
